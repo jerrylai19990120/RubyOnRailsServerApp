f701f1b570eb5337e6422bd2173c6bef88829b85
jerry <jerrylai19990120@gmail.com>
2021-01-12 12:27:18 -0500

    update

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module RailsServer
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.web_console.whitelisted_ips = '99.245.136.218'
+    config.api_only = true
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
