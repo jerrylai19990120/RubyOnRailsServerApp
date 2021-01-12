6db40df54a6d7d87373bedacd929f257f61942f7
jerry <jerrylai19990120@gmail.com>
2021-01-12 17:30:07 -0500

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users
+  
   
   get "/users", to: "users#index"
-  get "/users/:username", to: "users#show"
-  get "/users/getUser", to: "users#getUser"
   post "/users", to: "users#create"
+  get "/users/:username", to: "users#show"
+  
 end
